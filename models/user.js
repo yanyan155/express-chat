6a6c611dcea516d2ff0453e061051fb234223042
yanyan155 <jm@tut.by>
2019-05-09 21:41:23 +0300

    refactor: replace user login to mongoose schema logic

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('../libs/mongoose');
 const crypto = require('crypto');
+const HttpError = require('../error');
 
 const schema = new mongoose.Schema({
   name: {
@@ -36,6 +37,61 @@ schema.virtual('password').
     this.hashedPassword = this.encript(password);
   });
 
+schema.statics.authorize = function (username, password, cb) {
+  let User = this;
+  findUser('name', username)
+  .then(user => {
+    if(!user) {
+      let user = new User ({name: username, password: password});
+      saveUser(user)
+      .then(resolve => {
+        return cb(null, user);
+      })
+    } else {
+      if(user.checkPassword(password)) {
+        return cb(null, user);
+      } else { 
+        return cb(new HttpError(403, 'wrong password'));
+      }
+    }
+  })
+  .catch(err => cb(err));
+}
+
+
 const User = mongoose.model('User', schema);
 
 module.exports = User;
+
+function findUser(key , parametr) {
+
+  const find = {};
+  find[key] = parametr;
+
+  return new Promise(function(resolve, reject) {
+    User.findOne(find, (err, user) => {
+
+      if (err) {
+        reject(err);
+        closeConnection();
+      }else {
+        resolve(user);
+      };
+    })
+  });
+}
+
+function saveUser(user) {
+
+  return new Promise(function(resolve, reject) {
+
+    user.save((err, user) => {
+      if (err) {
+        reject(err);
+        closeConnection();
+      }else {
+        resolve(user);
+      };
+    })
+  });
+}
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const logger = require('../libs/logger');
-const mongoose = require('../libs/mongoose');
 const User = require('../models/user');
-const HttpError = require('../error');
 
 router.get('/', function(req, res, next) {
   res.render('index', { 
@@ -13,8 +11,6 @@ router.get('/', function(req, res, next) {
   });
 });
 
-// ПЕРЕНЕСТИ ЛОГИКУ В ЮЗЕРА
-
 router.post('/', function (req, res, next) {
   let bodyJson;
   for(key in req.body) {
@@ -22,60 +18,14 @@ router.post('/', function (req, res, next) {
   }
   let body = JSON.parse(bodyJson);
 
-  findUser('name', body.name)
-  .then(user => {
-    if(!user) {
-      let user = new User ({name: body.name, password: body.password});
-      saveUser(user)
-      .then(resolve => {
-
-        req.session.userId = user._id;
-        res.end();
-      })
+  User.authorize(body.name, body.password, function(err, user) {
+    if(err) {
+      next(err);
     } else {
-      if(user.checkPassword(body.password)) {
-
-        req.session.userId = user._id;
-        res.end();
-      } else {
-        next(new HttpError(403, 'wrong password'));
-      }
+      req.session.userId = user._id;
+      res.end();
     }
   })
-  .catch(err => next(err));
 })
 
 module.exports = router;
-
-function findUser(key , parametr) {
-
-  const find = {};
-  find[key] = parametr;
-
-  return new Promise(function(resolve, reject) {
-    User.findOne(find, (err, user) => {
-
-      if (err) {
-        reject(err);
-        closeConnection();
-      }else {
-        resolve(user);
-      };
-    })
-  });
-}
-
-function saveUser(user) {
-
-  return new Promise(function(resolve, reject) {
-
-    user.save((err, user) => {
-      if (err) {
-        reject(err);
-        closeConnection();
-      }else {
-        resolve(user);
-      };
-    })
-  });
-}
