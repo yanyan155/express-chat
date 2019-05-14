2260c4dda0f5bf6a3ff583b7ad1f2a721a76ee9f
yanyan155 <jm@tut.by>
2019-05-14 11:05:35 +0300

    feat: add enviroment var for mongodb

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('../config');
 
-mongoose.connect(config.get('mongoose:uri'), config.get('mongoose:options'))
+mongoose.connect(process.env.MONGOLAB_URI || config.get('mongoose:uri'), config.get('mongoose:options'))
 
 module.exports = mongoose;
